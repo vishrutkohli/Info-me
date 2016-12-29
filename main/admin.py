326ad60eafa5f90cb5c6d8ce0dc36ee294a9e1ec
vishrutkohli <kohlivishrut@gmail.com>
2016-12-29 16:24:01 +0530

    balle

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from main.models import organisations , fragmentname
-
-
-# Register your models here.
-admin.site.register(organisations)
-admin.site.register(fragmentname)
